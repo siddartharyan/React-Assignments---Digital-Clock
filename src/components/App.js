733e0dff7f7460aa3ba225d1f68fe86f3a102c48
siddartharyan <68399822+siddartharyan@users.noreply.github.com>
2020-10-30 22:06:29 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,22 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
-
-class App extends Component {
-    render() {
-
-        return(
-            <>
-               
-            </>
-        )
-    }
-}
-
-
+import React, { Component, useEffect, useRef, useState } from "react";
+import "../styles/App.css";
+const App = () => {
+  let d = new Date().toLocaleTimeString();
+  let [ctime, setTime] = useState(d);
+  let id = useRef(null);
+  useEffect(() => {
+    id.current = setInterval(() => {
+      let n = new Date().toLocaleTimeString();
+      setTime(n);
+    }, 1000);
+    return () => {
+      clearInterval(id);
+    };
+  });
+  return (
+    <div className="Clock">
+      <h3 id="time">{ctime}</h3>
+    </div>
+  );
+};
 export default App;
